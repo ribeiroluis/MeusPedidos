dc9f4e3a29f49ea5078d7d29a99b99d09060fb59
ribeiroluis <ribeiro.luis@hotmail.com>
2015-08-10 13:11:33 -0300

    changes

--- controllers/questionnaireController.js
+++ controllers/questionnaireController.js
@@ -1,28 +1,32 @@
 ﻿myApp.controller('questionnaireController', ['$scope', function ($scope) {
-    $scope.tecniches = [
-        { name: "HTML", value: 0 },
-        { name: "CSS", value: 0 },
-        { name: "Javascript", value: 0 },
-        { name: "Python", value: 0 },
-        {name: "Django", value: 0},
-        {name: "Desenvolvimento iOS", value: 0},
-        {name: "Desenvolvimento Android", value: 0},
+    $scope.techniques = [
+        {
+            group: "FronEnd",
+            techniques: [{ name: "HTML", value: 0 }, { name: "CSS", value: 0 }, { name: "Javascript", value: 0 }]
+        }, {
+            group: "BackEnd",
+            techniques: [{ name: "Python", value: 0 }, { name: "Django", value: 0 }]
+        }, {
+            group: "Mobile",
+            techniques: [{ name: "Desenvolvimento iOS", value: 0 }, { name: "Desenvolvimento Android", value: 0 }]
+        }
     ];
 
 
+
     $scope.submit = function () {
-       var habilitys = this.tecniches.filter(function(e){
-            if(e.value >= 7){                
+        var habilitys = this.tecniches.filter(function (e) {
+            if (e.value >= 7) {
                 return e;
-            }           
-           });
-        habilitys.forEach(function(e){
-            if(e.name == "")
-            
-            
-            
-            console.log(e);
-            });
+            }
+        });
+        habilitys.forEach(function (e) {
+            if (e.name == "")
+
+
+
+                console.log(e);
+        });
       
         
         //alert('test');

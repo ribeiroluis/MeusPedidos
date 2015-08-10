88b7c894c60451263e7e0a5ac9b24d4031e33933
ribeiroluis <ribeiro.luis@hotmail.com>
2015-08-10 07:15:03 -0300

    initial commit

---
+++ controllers/questionnaireController.js
@@ -0,0 +1,20 @@
+﻿myApp.controller('questionnaireController', ['$scope', function ($scope) {
+    $scope.tecniches = [
+        { name: "HTML", value: 0 },
+        { name: "CSS", value: 0 },
+        { name: "Javascript", value: 0 },
+        { name: "Python", value: 0 }
+    ];
+
+
+    $scope.submit = function () {
+        alert('test');
+        var link = "mailto:ribeiro.luis@hotmail.com"                 
+                 + "&subject=" + escape("This is my subject")
+                 + "&body=" + escape("This is my body")
+        ;
+        window.location.href = link;
+    }
+
+
+}]);

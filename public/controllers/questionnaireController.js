186ae19dd215e934b1372c0c9f247eb83eb24267
Luis Ribeiro <ribeiro.luis@hotmail.com>
2015-08-10 21:23:10 -0300

    install express

---
+++ public/controllers/questionnaireController.js
@@ -0,0 +1,41 @@
+﻿myApp.controller('questionnaireController', ['$scope', function ($scope) {
+    $scope.techniques = [
+        {
+            group: "FronEnd",
+            techniques: [{ name: "HTML", value: 0 }, { name: "CSS", value: 0 }, { name: "Javascript", value: 0 }],
+        }, {
+            group: "BackEnd",
+            techniques: [{ name: "Python", value: 0 }, { name: "Django", value: 0 }],                        
+        }, {
+            group: "Mobile",
+            techniques: [{ name: "Desenvolvimento iOS", value: 0 }, { name: "Desenvolvimento Android", value: 0 }],                        
+        }
+    ];
+
+
+
+    $scope.submit = function () {
+        var habilitys = this.tecniches.filter(function (e) {
+            if (e.value >= 7) {
+                return e;
+            }
+        });
+        habilitys.forEach(function (e) {
+            if (e.name == "")
+
+
+
+                console.log(e);
+        });
+      
+        
+        //alert('test');
+        // var link = "mailto:ribeiro.luis@hotmail.com"                 
+        //          + "&subject=" + escape("This is my subject")
+        //          + "&body=" + escape("This is my body")
+        // ;
+        window.location.href = link;
+    }
+
+
+}]);

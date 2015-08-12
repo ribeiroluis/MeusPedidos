894570e6545ff080cc2a0a3c2ed642f1d44ed4cf
Luis Ribeiro <ribeiro.luis@hotmail.com>
2015-08-11 23:08:34 -0300

    send email correction

--- public/controllers/questionnaireController.js
+++ public/controllers/questionnaireController.js
@@ -1,31 +1,63 @@
 ﻿/* global myApp */
 myApp.controller('questionnaireController', ['$scope', '$http', function ($scope, $http) {
-    var _dom = new utilsDom();    
-    $scope.techniques = { 
-        "FronEnd": [{ name: "HTML", value: 0 }, { name: "CSS", value: 0 }, { name: "Javascript", value: 0 }],
-        "BackEnd": [{ name: "Python", value: 0 }, { name: "Django", value: 0 }],
-        "Mobile": [{ name: "Desenvolvimento iOS", value: 0 }, { name: "Desenvolvimento Android", value: 0 }]};
+    var _dom = new utilsDom();
+    var emailsToSent = [];
+
+    $scope.candidateInformation = {
+        post: "",
+        email: "",
+        name: ""
+    };
+
+    $scope.techniques = [
+        {
+            post: "FronEnd",
+            techniques: [{ name: "HTML", value: 0 }, { name: "CSS", value: 0 }, { name: "Javascript", value: 0 }]
+        }, {
+            post: "BackEnd",
+            techniques: [{ name: "Python", value: 0 }, { name: "Django", value: 0 }]
+        }, {
+            post: "Mobile",
+            techniques: [{ name: "Desenvolvimento iOS", value: 0 }]
+        }, {
+            post: "Mobile",
+            techniques: [{ name: "Desenvolvimento Android", value: 0 }]
+        }];
+
     $scope.submit = function () {
-        var emailsToSent = [];
-        $.each(this.techniques, function(index){
-            if(this.techniques[index].filter(function(e){if(e.value >= 7)return e}).length < this.techniques[index].length){
-               emailsToSent.push(index); 
+        var posts = [];
+        this.techniques.forEach(function (element, index, array) {
+            if (element.techniques.filter(function (e) {
+                if (e.value >= 7) return e;
+            }).length == element.techniques.length) {
+                if (posts.indexOf(element.post) < 0) {
+                    posts.push(element.post);
+                }
             }
         });
-        
-        
-        //alert('test');
-        // var link = "mailto:ribeiro.luis@hotmail.com"                 
-        //          + "&subject=" + escape("This is my subject")
-        //          + "&body=" + escape("This is my body")
-        // ;
-        //window.location.href = link;
+        if (posts.length < 1) {
+            posts.push("");
+        }
+        for (var i = 0; i < posts.length; i++) {
+            emailsToSent.push({
+                post: posts[i],
+                name: this.candidateInformation.name,
+                email: this.candidateInformation.email,
+                subject: "Obrigado por se candidatar",
+                emailText: "Obrigado por se candidatar, assim que tivermos uma vaga disponível para programador " + posts[i] + " entraremos em contato."
+            });
+        }
+        sendEmail(emailsToSent.pop());
     }
-    function sentEmail(data){
+
+    function sendEmail (data) {
         $http.post('/sendEmail', data).success(function (data) {
-            console.log(data);            
+            console.log(data);
+            if(emailsToSent.length> 0){
+                sendEmail(emailsToSent.pop());
+            }                        
         }).error(function (data) {
-            console.log('Error: ' + data);                        
-        });        
+            console.log('Error: ' + data);
+        });
     }
 }]);
--- server.js
+++ server.js
@@ -1,45 +1,46 @@
-var express  = require('express');
-var app      = express(); 					// create our app w/ express
-var port  	 = process.env.PORT || 3000; 	// set the port
-var bodyParser = require('body-parser'); 	// pull information from HTML POST (express4)
+var express = require('express');
+var app = express(); 					      // create our app w/ express
+var port = process.env.PORT || 3000; 	      // set the port
+var bodyParser = require('body-parser'); 	  // pull information from HTML POST (express4)
 var nodemailer = require('nodemailer');
 
 app.use(express.static(__dirname + '/public')); 				// set the static files location /public/img will be /img for users
-app.use(bodyParser.urlencoded({'extended':'true'})); 			// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ 'extended': 'true' })); 		// parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); 									// parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 
 var transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-        user: 'juanbondbr@gmail.com',
-        pass: '..'
-    }
-});
-var mailOptions = {
-    from: 'Luis Ribeiro ✔ <juanbondbr@gmail.com>', // sender address
-    to: 'ribeiro.luis@hotmail.com', // list of receivers
-    subject: 'Hello ✔', // Subject line
-    text: 'Hello world ✔', // plaintext body
-    html: '<b>Hello world ✔</b>' // html body
-};
+        service: 'Gmail',
+        auth: {
+            user: '@gmail.com',
+            pass: ''
+        }
+    });
 
 // GET method route
 app.get('/get', function (req, res) {
-  console.log("get");
-  res.send('GET request to the homepage');
+    console.log("get");
+    res.send('GET request to the homepage');
 });
 
 // POST method route
 app.post('/sendEmail', function (req, res) {
-  transporter.sendMail(mailOptions, function(error, info){
-    if(error){
-        res.status(500).send('Something broke!');
-        return console.error(error);
-    }
-    res.status(200).send('Email sent OK!');
-    console.info('Message sent: ' + info.response);   
-});
+    var mailOptions = {
+        from: 'Luis Ribeiro <@gmail.com>', // sender address
+        to: req.body.email, // list of receivers
+        subject: req.body.subject, // Subject line
+        text: req.body.emailText, // plaintext body
+        //html: '<b>'+req.body.emailText+'</b>' // html body
+    };   
+    var body = req.body;
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            res.status(500).send('Something broke!');
+            return console.error(error);
+        }
+        res.status(200).send('Email sent OK!');
+        console.info('Message sent: ' + info.response);
+    });
 });
 
 app.listen(port);
